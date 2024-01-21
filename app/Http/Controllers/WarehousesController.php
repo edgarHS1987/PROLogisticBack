ba93aec95e7320e4d9e5e2c6fe9a48e65efd893c
edgarhs1987 <edgar.hmr@gmail.com>
2024-01-21 14:23:28 -0600

    correccion en seed

--- app/Http/Controllers/WarehousesController.php
+++ app/Http/Controllers/WarehousesController.php
@@ -10,12 +10,16 @@ use Exception;
 class WarehousesController extends Controller
 {
     public function show($id){
-        $warehouses = Clients::join('clients_warehouses', 'clients_warehouses.clients_id', 'clients.id')
+        try {
+            $warehouses = Clients::join('clients_warehouses', 'clients_warehouses.clients_id', 'clients.id')
                         ->join('warehouses', 'warehouses.id', 'clients_warehouses.warehouses_id')
+                        ->select('warehouses.id as value', \DB::raw('CONCAT(warehouses.name, " - ", clients.name) as label'))
                         ->selectRaw('warehouses.id as value, CONCAT(warehouses.name, " - ", clients.name) as label')
                         ->where('clients.id', $id)
                         ->get();
-
+        } catch (\Exception $e) {
+            return response()->json(['error'=>'ERROR ('.$e->getCode().'): '.$e->getMessage()]);
+        }
         return response()->json($warehouses);
     }
 
