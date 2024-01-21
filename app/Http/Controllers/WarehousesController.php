55c03b540ee5b2a7ae8047fcdd1e3ef425ae6270
edgarhs1987 <edgar.hmr@gmail.com>
2024-01-21 14:29:24 -0600

    correccion en seed

--- app/Http/Controllers/WarehousesController.php
+++ app/Http/Controllers/WarehousesController.php
@@ -14,7 +14,6 @@ class WarehousesController extends Controller
             $warehouses = Clients::join('clients_warehouses', 'clients_warehouses.clients_id', 'clients.id')
                         ->join('warehouses', 'warehouses.id', 'clients_warehouses.warehouses_id')
                         ->select('warehouses.id as value', \DB::raw('CONCAT(warehouses.name, " - ", clients.name) as label'))
-                        ->selectRaw('warehouses.id as value, CONCAT(warehouses.name, " - ", clients.name) as label')
                         ->where('clients.id', $id)
                         ->get();
         } catch (\Exception $e) {
